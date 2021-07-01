0fdc4712a8d8972f4f2b44c76ec960a45ba02e4c
123qpq <45002168+123qpq@users.noreply.github.com>
2021-07-01 09:18:21 +0900

    Create cost func in tensor.py

---
+++ cost func in tensor.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+X = np.array([1, 2, 3])
+Y = np.array([1, 2,3 ])
+
+def cost_func(W, X, Y): # 1/m * sigma{(WX-Y)^2}
+    hypothesis = X * W
+    return tf.reduce_mean(tf.square(hypothesis - Y))
+
+W_values = np.linspace(-3, 5, num=15)
+cost_values = []
+
+for feed_W in W_values:
+    curr_cost = cost_func(feed_W, X, Y)
+    cost_values.append(curr_cost)
+    print("{:6.3f} | {:10.5f}".format(feed_W, curr_cost))
