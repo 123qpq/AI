cbcb4a44a140c8bde0371d10b655c1a20ec28823
kyopark <123qpq@naver.com>
2021-07-26 23:46:33 +0900

    Update detect face2.py

--- detect face2.py
+++ detect face2.py
@@ -1,6 +1,14 @@
+from numpy.core.fromnumeric import shape
 import cv2, dlib
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+eyes = list(range(36, 48))
+reye = list(range(36, 42))
+reye = list(range(42, 48))
+nose = list(range(27, 36))
+mouth = list(range(48, 68))
 
 
 scaler = 0.3
@@ -13,6 +21,7 @@ cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     if(success):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #img = cv2.resize(img, (int(img.shape[1] * scaler), int(img.shape[0] * scaler)))
         ori = img.copy()
 
@@ -24,7 +33,12 @@ while True:
         cv2.rectangle(img, (face.left(), face.top()), (face.right(), face.bottom()), (0, 255, 0), 5) #이미지, 시작점, 정료점, 색상, 선두께
         dlib_shape = predictor(img, face)
         shape_2d = np.array([[p.x, p.y] for p in dlib_shape.parts()])
-
+        '''
+        showpart = shape_2d[0][36:48]
+        print(shape_2d)
+        for s in showpart:
+            cv2.circle(img, center=tuple(s), radius=1, color=(255, 255, 255), thickness = 2)
+        '''
         for s in shape_2d:
             cv2.circle(img, center=tuple(s), radius=1, color=(255, 255, 255), thickness = 2)
 
